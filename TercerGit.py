9ebd68a581ba369d5fa8524d4ffd02ce018c57f2
Sneydder <sneyddernavarro09@gmail.com>
2022-09-13 23:49:15 -0500

    Bases de Python, pruebas en el body de la pagina

--- TercerGit.py
+++ TercerGit.py
@@ -1,2 +1,22 @@
-"Tercer Hola Mundo"
-"Status Alternativo"
+from ast import If
+from operator import ifloordiv
+
+nombre = input('¿Cual es tu nombre?')
+
+edad = int(input('¿Cuantos años tienes?'))
+
+InformaciónPersonal= 'Hola mundo, soy ' + nombre + ' y tengo ' + str(edad) + ' años '
+print (InformaciónPersonal)
+
+numero1 = 25
+numero2 = 24
+
+if numero1 > numero2:
+    print('el numero 1 es mayor al numero 2')
+elif numero2 > numero1:
+    print('el numero 2 es mayor que el numero 1')
+
+if edad >= 18:
+    print('ES MAYOR DE EDAD')
+elif edad <= 17:
+    print('ES MENOR DE EDAD')
