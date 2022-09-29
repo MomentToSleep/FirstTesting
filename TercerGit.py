f30c5663f5f5c1ea63218a8a7d558aaefeaf7b79
Sneydder <sneyddernavarro09@gmail.com>
2022-09-29 01:47:35 -0500

    ONLY SXERCISE

--- TercerGit.py
+++ TercerGit.py
@@ -1,34 +1,5 @@
-<<<<<<< HEAD
-nombre = input('¿Cual es tu nombre?')
-
-edad = int(input('¿Cuantos años tienes?'))
-
-InformaciónPersonal= 'Hola mundo, soy ' + nombre + ' y tengo ' + str(edad) + ' años '
-print (InformaciónPersonal)
-
-numero1 = 25
-numero2 = 24
-
-if numero1 > numero2:
-    print('el numero 1 es mayor al numero 2')
-elif numero2 > numero1:
-    print('el numero 2 es mayor que el numero 1')
-
-if edad >= 18:
-    print( nombre + ' ES MAYOR DE EDAD')
-elif edad <= 17:
-    print(nombre + ' ES MENOR DE EDAD')
-
-    print('ES MENOR DE EDAD')
-
-if edad> 60:
-    print("Es de la tercera edad")
-else:
-    print("No es de la tercera edad")
-
-=======
 nombre = input("Cual es tu nombre?")
 edad = input("Cual es tu edad")
 direccion = input("Dirección de Residencia")
 print ('Hola ' + nombre + ' usted tiene ' + str(edad) + ' años y su dirección es ' + str(direccion))
->>>>>>> Testing
+
