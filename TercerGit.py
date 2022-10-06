c7ce6c33cf850b8c88fd64145821c26fdec8c684
Sneydder <sneyddernavarro09@gmail.com>
2022-10-06 03:13:35 -0500

    Learning

--- TercerGit.py
+++ TercerGit.py
@@ -1,4 +1,11 @@
 nombre = input("Cual es tu nombre?")
-edad = input("Cual es tu edad")
-direccion = input("Dirección de Residencia")
-print ('Hola ' + nombre + ' usted tiene ' + str(edad) + ' años y su dirección es ' + str(direccion))
+print ('Hola ' + nombre)
+edad = int(input("Cual es tu edad"))
+Cumple = edad >= 15
+MessHijoDelDueño = input ('Eres el hjo/a del dueño?')
+CumpleToo = MessHijoDelDueño
+HijoDelDueño = edad == 'si'
+if Cumple:
+  print ( 'Usted tiene ' + str(edad) + ' Usted puede ingresar')
+else:
+  print ( 'Usted tiene ' + str(edad) + ' Usted no puede ingresar')
