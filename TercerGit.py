eff9363a06c5d3fbf7a255a4eee43cc657ccf5d0
Sneydder <sneyddernavarro09@gmail.com>
2022-10-06 03:16:36 -0500

    TercerGit.py

--- TercerGit.py
+++ TercerGit.py
@@ -1,10 +1,4 @@
 nombre = input("Cual es tu nombre?")
-<<<<<<< HEAD
-edad = input("Cual es tu edad")
-direccion = input("Dirección de Residencia")
-print ('Hola ' + nombre + ' usted tiene ' + str(edad) + ' años y su dirección es ' + str(direccion))
-
-=======
 print ('Hola ' + nombre)
 edad = int(input("Cual es tu edad"))
 Cumple = edad >= 15
@@ -15,4 +9,3 @@ if Cumple:
   print ( 'Usted tiene ' + str(edad) + ' Usted puede ingresar')
 else:
   print ( 'Usted tiene ' + str(edad) + ' Usted no puede ingresar')
->>>>>>> Testing
